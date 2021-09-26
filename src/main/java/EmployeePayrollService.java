51df1b9dc6a385fdb64bc9f017b3d1de0896a4f8
PanchamiShenoy <panchamishenoy16@gmail.com>
2021-09-26 21:36:48 +0530

    method to write employee pay roll to file

--- src/main/java/EmployeePayrollService.java
+++ src/main/java/EmployeePayrollService.java
@@ -1,8 +1,11 @@
 import java.util.*;
 
 public class EmployeePayrollService {
+	public enum IOService {
+		CONSOLE_IO, FILE_IO, DB_IO, REST_IO
+	}
 
-	private List<EmployeePayrollData> employeePayrollList;
+	public List<EmployeePayrollData> employeePayrollList;
 
 	public EmployeePayrollService() {
 		System.out.println("Welcome to Employee Payroll Service.");
@@ -15,6 +18,17 @@ public class EmployeePayrollService {
 	/*
 	 * method to read employee detail from the user
 	 */
+	public void writeEmployeePayrollData(EmployeePayrollService.IOService fileIo) {
+		if (fileIo.equals(IOService.CONSOLE_IO)) {
+			System.out.println("\n Writing Employee Payroll Roaster to Console\n" + employeePayrollList);
+		} else if (fileIo.equals(IOService.FILE_IO)) {
+			new EmployeePayrollFileIOService().writeData(employeePayrollList);
+		}
+	}
+	public long countEntries(IOService ioService)
+	{  
+        return new EmployeePayrollFileIOService().countEntries();
+    }
 	private void readEmployeePayrollData(Scanner consoleInputReader) {
 		System.out.println("Enter employee ID: ");
 		int id = consoleInputReader.nextInt();
@@ -38,6 +52,7 @@ public class EmployeePayrollService {
 		Scanner consoleInputReader = new Scanner(System.in);
 		employeePayrollService.readEmployeePayrollData(consoleInputReader);
 		employeePayrollService.writeEmployeePayrollData(consoleInputReader);
+		employeePayrollService.writeEmployeePayrollData(IOService.FILE_IO);
 		consoleInputReader.close();
 	}
 }
---
+++ src/test/java/EmployeePayrollServiceTest.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+/**
+ * Employee Payroll Service Test to check the file outputs
+ */
+public class EmployeePayrollServiceTest {
+	@Test
+	public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+		EmployeePayrollData[] arrayOfEmps = { new EmployeePayrollData(1, " Jeff", 100000.0),
+				new EmployeePayrollData(2, "Bill", 200000.0), new EmployeePayrollData(3, "Mark", 300000.0) };
+		EmployeePayrollService employeePayrollService;
+		employeePayrollService = new EmployeePayrollService(Arrays.asList(arrayOfEmps));
+		employeePayrollService.writeEmployeePayrollData(EmployeePayrollService.IOService.FILE_IO);
+		long entries = employeePayrollService.countEntries(EmployeePayrollService.IOService.FILE_IO);
+		assertEquals(3, entries);
+	}
+}
